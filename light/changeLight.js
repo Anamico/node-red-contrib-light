4a202443b679425c2a063eb4e4f033c235e3484f
Macinspak <Manager@ImagineIT.com.au>
2018-12-20 22:10:40 +1000

    switch to hsl for LIFX

--- light/changeLight.js
+++ light/changeLight.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const convert = require('color-convert');
+
 module.exports = function(RED) {
 
     function AnamicoChangeLight(config) {
@@ -20,40 +22,6 @@ module.exports = function(RED) {
             return;
         }
 
-        /**
-         * handle inputs
-         */
-        node.on('input', function(msg) {
-            node.log(msg);
-
-            // if (this.configError) { return; }  // ignore everything if in error state, can only redeploy to fix this state
-            //
-            // // error if not in homekit mode and we get a homekit message
-            // if (!this.fromHomekit && msg.hap && msg.hap.context) {
-            //     node.error('homekit message received when not in homekit mode', msg);
-            //     node.status({ fill: "red", shape: "dot", text: "homekit message received" });
-            //     this.configError = true;
-            //     return
-            // }
-            //
-            // // silently ignore non-homekit messages (if we are expecting homekit) to avoid loops
-            // if (this.fromHomekit && !(msg.hap && msg.hap.context)) {
-            //     return
-            // }
-            // delete msg.hap; // make sure we remove "hap" details to avoid future problems down the track.
-            //
-            // if (!msg.payload) {
-            //     node.error('empty message received', msg);
-            //     node.status({ fill: "red", shape: "dot", text: "empty message received" });
-            //     return
-            // }
-            //
-            // node.status({ fill: "blue", shape:"dot", text: "updating panel..." });
-            // node._panel.setState(msg, function(result) {
-            //     node.status({ fill: result.error ? "red" : "green", shape: "dot", text: result.label });
-            // });
-        });
-
 
         /**
          * listen for panel state changes
@@ -66,9 +34,13 @@ module.exports = function(RED) {
                 text:   msg.payload.on ? (msg.payload.luminance ? ((msg.payload.luminance/100) * 100).toFixed(2) + '%' : 'on') : 'off'
             });
             // reformat the command to match the type of light
-
-
-
+            if ((['Basic','LIFX'].indexOf(config.mode) > -1) && (typeof msg.payload.hex !== 'undefined')) {
+                const hsl = convert.hex.hsv(msg.payload.hex);
+                msg.payload.hue = hsl[0];
+                msg.payload.sat = hsl[1];
+                msg.payload.bri = hsl[2];
+                delete msg.payload.hex;
+            }
             node.send(msg);
         });
 
